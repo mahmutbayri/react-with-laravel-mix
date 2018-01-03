819abbac5cffa51e742ae96f9f8ff9223f74bd17
Mahmut Bayri <mahmutbayri@gmail.com>
2018-01-03 12:13:54 +0000

    Updated 'generating table sample'

--- src/examples/generate_table.js
+++ src/examples/generate_table.js
@@ -1,47 +1,111 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
-function TableGeneratorWithString() {
-    let generateTable = function (row = 4, col = 4) {
-        let td = '',
-            tr = '';
-        for (let i = 0; i < row; i++) {
-            for (let j = 0; j < col; j++) {
-                if (j === 0) {
-                    td += '<th>Row: ' + i + '</th>';
-                }
-                td += '<td>Hücre ' + (new Date()).toISOString() + j + '</td>'
-            }
-            tr += '<tr>' + td + '</tr>';
-            td = '';
-        }
-        return '<table border="1">' + tr + '</table>';
-    };
-    return <div dangerouslySetInnerHTML={{__html: generateTable(2, 2)}}></div>;
-}
+class TableGenerator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            row: 4,
+            col: 5
+        };
+    }
 
-class TableGeneratorWithJsx extends Component {
+    updateTable(options) {
+        console.log(options);
+        this.setState((prevState, props) => {
+            return options;
+        })
+    }
+
     render() {
+
         let datem = (new Date()).toString();
-        return <table>
-            <caption>TableGeneratorWithJsx</caption>
-            <tbody>
-            <tr>
-                <td>{datem}</td>
-            </tr>
-            </tbody>
-        </table>;
+
+        return <div className="container">
+            <div className="card mb-3">
+                <div className="card-body">
+                    {datem}
+                </div>
+            </div>
+            <div className="card mb-3">
+                <div className="card-body">
+                    <Form passedVal={this.state} onUpdate={this.updateTable.bind(this)}/>
+                </div>
+            </div>
+            <div className="card mb-3">
+                <div className="card-body">
+                    <TableGeneratorWithJsx passedVal={this.state}/>
+                </div>
+            </div>
+        </div>
     }
 }
 
-function tick() {
-    ReactDOM.render(
-        <div>
-            <TableGeneratorWithJsx/>
-            <hr/>
-        </div>,
-        document.getElementById('root')
-    );
+class TableGeneratorWithJsx extends Component {
+
+    render() {
+        // let rowCount = this.state.rowCount;
+        // let colCount = this.state.colCount;
+
+        let rowCount = this.props.passedVal.row;
+        let colCount = this.props.passedVal.col;
+
+        console.log(this.props, "TableGeneratorWithJsx");
+
+        let headCols = [];
+
+        let bodyRows = [];
+        let bodyCols = [];
+
+        for (let i = 1; i <= rowCount; i++) {
+            for (let j = 1; j <= colCount; j++) {
+                bodyCols.push(<td key={i + '-' + j}>Cell {i * j}</td>);
+                if (i === 1) {
+                    headCols.push(<th key={i + '-' + j}>Title {j}</th>)
+                }
+            }
+            bodyRows.push(<tr key={i}>{bodyCols}</tr>);
+            bodyCols = [];
+        }
+        return <table className="table table-striped">
+            <thead>
+            <tr>{headCols}</tr>
+            </thead>
+            <tbody>
+            {bodyRows}
+            </tbody>
+        </table>
+    }
 }
 
-setInterval(tick, 1000);
+class Form extends Component {
+    handleSubmit(event) {
+        event.preventDefault();
+
+        this.props.onUpdate({
+            col: this.colInput.value,
+            row: this.rowInput.value
+        })
+    }
+
+    render() {
+        return <form onSubmit={this.handleSubmit.bind(this)}>
+            <div className="form-group">
+                <label>Columns</label>
+                <input className="form-control" defaultValue={this.props.passedVal.col} ref={input => this.colInput = input}/>
+            </div>
+            <div className="form-group">
+                <label>Rows</label>
+                <input className="form-control" defaultValue={this.props.passedVal.row} ref={input => this.rowInput = input}/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+    }
+}
+
+ReactDOM.render(
+    <TableGenerator/>,
+    document.getElementById('root')
+);
+
+// setInterval(tick, 1000);
