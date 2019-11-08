ac9b2d1e6977870e939068a530908619064800e5
Mahmut Bayri <mahmutbayri@gmail.com>
2019-11-08 09:23:24 +0300

    Added watch option for webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,12 @@ const mix = require('laravel-mix');
 
 // mix.config.resourceRoot = 'src';
 
+mix.config.webpackConfig.watchOptions = {
+    aggregateTimeout: 500,
+    poll: 1000,
+    ignored: /node_modules/
+};
+
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
